016b78eab72985459e0d9474f116642da340f2d0
Vinay Hiremath <github@vinayh.com>
2023-09-16 19:59:57 +0100

    Added ability to view, edit, delete individual entries (eventually corresponding to weeks)

--- app.py
+++ app.py
@@ -1,5 +1,17 @@
 import sqlite3
-from flask import Flask, render_template
+import sys
+from pathlib import Path
+from flask import Flask, render_template, request, url_for, flash, redirect
+from werkzeug.exceptions import abort
+
+
+def get_secret_key():
+    FLASK_SECRET_PATH = Path(".flask_secret")
+    try:
+        with FLASK_SECRET_PATH.open("r") as flask_secret_file:
+            return flask_secret_file.read()
+    except:
+        sys.exit()
 
 
 def get_db_connection():
@@ -7,7 +19,18 @@ def get_db_connection():
     conn.row_factory = sqlite3.Row
     return conn
 
+
+def get_entry(entry_id: int):
+    conn = get_db_connection()
+    entry = conn.execute('SELECT * FROM entries WHERE id = ?', (entry_id,)).fetchone()
+    conn.close()
+    if entry is None:
+        abort(404)
+    return entry
+
+
 app = Flask(__name__)
+app.config['SECRET_KEY'] = get_secret_key()
 
 
 @app.route('/')
@@ -16,8 +39,66 @@ def index():
     entries = conn.execute('SELECT * FROM entries').fetchall()
     conn.close()
     return render_template('index.html', entries=entries)
-# def hello_world():  # put application's code here
-#     return 'Hello World 3!'
+
+
+@app.route('/<int:entry_id>')
+def entry(entry_id):
+    entry = get_entry(entry_id)
+    return render_template('entry.html', entry=entry)
+
+
+@app.route('/add', methods=('GET', 'POST'))
+def add():
+    if request.method == 'POST':
+        start = request.form['start']
+        category = request.form['category']
+        note = request.form['note']
+        if not start:
+            flash('Start date is required!')
+        if not category:
+            flash('Category is required!')
+        if start and category:
+            conn = get_db_connection()
+            conn.execute('INSERT INTO entries (start, category, note) VALUES (?, ?, ?)',
+                         (start, category, note))
+            conn.commit()
+            conn.close()
+            return redirect(url_for('index'))
+
+    return render_template('add.html')
+
+
+@app.route('/<int:entry_id>/edit', methods=('GET', 'POST'))
+def edit(entry_id):
+    entry = get_entry(entry_id)
+    if request.method == 'POST':
+        start = request.form['start']
+        category = request.form['category']
+        note = request.form['note']
+        if not start:
+            flash('Start date is required!')
+        if not category:
+            flash('Category is required!')
+        if start and category:
+            conn = get_db_connection()
+            conn.execute('UPDATE entries SET start = ?, category = ?, note = ? WHERE id = ?',
+                         (start, category, note, entry_id))
+            conn.commit()
+            conn.close()
+            return redirect(url_for('index'))
+
+    return render_template('edit.html', entry=entry)
+
+
+@app.route('/<int:entry_id>/delete', methods=('POST',))
+def delete(entry_id):
+    entry = get_entry(entry_id)
+    conn = get_db_connection()
+    conn.execute('DELETE FROM entries WHERE id = ?', (entry_id,))
+    conn.commit()
+    conn.close()
+    flash(f'Entry starting on {entry["start"]} was successfully deleted!')
+    return redirect(url_for('index'))
 
 
 if __name__ == '__main__':
