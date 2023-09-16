4a4e0b37fefe927c7c41edfbca4e20bf3daf87b1
Vinay Hiremath <github@vinayh.com>
2023-09-16 14:05:28 +0100

    Added Bootstrap 5.3.1 and base template

--- app.py
+++ app.py
@@ -1,11 +1,13 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 
 @app.route('/')
-def hello_world():  # put application's code here
-    return 'Hello World!'
+def index():
+    return render_template('index.html')
+# def hello_world():  # put application's code here
+#     return 'Hello World 3!'
 
 
 if __name__ == '__main__':
